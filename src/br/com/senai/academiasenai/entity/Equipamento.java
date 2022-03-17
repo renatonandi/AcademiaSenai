eac3a1bbfa8c688c4f5a52327e5f30a32f4ad2af
Renato Nandi Neto <renato.nandi@gmail.com>
2022-03-17 20:09:38 -0300

    Inicio do desenvolvimento dos recursos de gestão de instrutores de equipamentos.

---
+++ src/br/com/senai/academiasenai/entity/Equipamento.java
@@ -0,0 +1,5 @@
+package br.com.senai.academiasenai.entity;
+
+public class Equipamento {
+
+}
---
+++ src/br/com/senai/academiasenai/entity/Instrutor.java
@@ -0,0 +1,10 @@
+package br.com.senai.academiasenai.entity;
+
+public class Instrutor {
+	
+	private Integer id;
+	
+	private String nomeCompleto;
+	
+	
+}
