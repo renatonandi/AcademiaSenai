c988b33e4346005511808ac3479605a4eef3c295
Renato Nandi Neto <renato.nandi@gmail.com>
2022-03-17 20:38:41 -0300

    Ajuste do nome da entidade de negócio

---
+++ src/br/com/senai/academiasenai/entity/Professor.java
@@ -0,0 +1,28 @@
+package br.com.senai.academiasenai.entity;
+
+public class Professor {
+	
+	private Integer id;
+	
+	private String nomeCompleto;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	
+	
+	
+	
+}
